c9ff9878801598036b0f2ffe8bb6d49b9b1aa5fd
kipropbrian <kipropbrian@gmail.com>
2024-10-06 22:02:51 +0200

    efficiency changes

--- src/partials/HeaderNav.vue
+++ src/partials/HeaderNav.vue
@@ -5,7 +5,7 @@
 			<div class="flex justify-between items-center px-4 space-x-8 sm:-my-px sm:ml-10 sm:flex">
 				<div class="flex shrink-0">
 					<router-link to="/">
-						<img class="w-20 h-20" src="/facevector.jpg" alt="face mesh" />
+						<img class="w-20 h-20" src="/facevector.webp" alt="face mesh" />
 					</router-link>
 				</div>
 				<nav class="flex h-16 px-4 py-2 items-center hover:bg-gray-100 hover:shadow-sm">
