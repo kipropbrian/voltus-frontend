e9268ea4ec9b45692b19186f2a8d330cfa19348e
kipropbrian <kipropbrian@gmail.com>
2024-11-30 17:17:43 +0200

    adding shadcn

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import { fileURLToPath, URL } from 'node:url';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		postcss: {
+			plugins: [tailwind(), autoprefixer()],
+		},
+	},
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./', import.meta.url)),
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
 });
